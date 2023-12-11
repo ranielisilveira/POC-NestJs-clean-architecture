c14085fa0d9c86908d3b49ff46e8a8e9eb19b160
Ranieli <ranielisilveira@hotmail.com>
2023-12-11 08:04:55 -0300

    feat: Register new providers

--- src/projects/projectsModule.ts
+++ src/projects/projectsModule.ts
@@ -1,12 +1,26 @@
 import { Module } from '@nestjs/common';
 import { ProjectsService } from './projectsService';
-import { ProjectsController } from './projectsController';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Project } from './entities/projectEntity';
+import { ProjectsWithUseCaseController } from './projectsWithUseCaseController';
+import { CreateProjectUseCase } from './useCases/createProjectUseCase';
+import { FindAllProjectsUseCase } from './useCases/findAllProjectsUseCase';
+import { StartProjectUseCase } from './useCases/startProjectUseCase';
+import { ProjectTypeOrmRepository } from './projectRespository';
 
 @Module({
   imports: [TypeOrmModule.forFeature([Project])],
-  controllers: [ProjectsController],
-  providers: [ProjectsService],
+  controllers: [ProjectsWithUseCaseController],
+  providers: [
+    ProjectsService,
+    CreateProjectUseCase,
+    FindAllProjectsUseCase,
+    StartProjectUseCase,
+    ProjectTypeOrmRepository,
+    {
+      provide: 'IProjectRepository',
+      useExisting: ProjectTypeOrmRepository,
+    },
+  ],
 })
 export class ProjectsModule {}
