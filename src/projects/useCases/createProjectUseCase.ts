6609a967abc858d96ca43bdf26d3c2f90d9b1168
Ranieli <ranielisilveira@hotmail.com>
2023-12-11 08:03:30 -0300

    feat: create method create

---
+++ src/projects/useCases/createProjectUseCase.ts
@@ -0,0 +1,18 @@
+import { CreateProjectDto } from '../dto/createProjectDto';
+import { Project } from '../entities/projectEntity';
+import { Inject, Injectable } from '@nestjs/common';
+import { IProjectRepository } from '../projectRespository';
+
+@Injectable()
+export class CreateProjectUseCase {
+  constructor(
+    @Inject('IProjectRepository')
+    private readonly projectRepository: IProjectRepository, //Repository em memoria
+  ) {}
+
+  async execute(input: CreateProjectDto) {
+    const project = new Project(input);
+    await this.projectRepository.create(project);
+    return project;
+  }
+}
